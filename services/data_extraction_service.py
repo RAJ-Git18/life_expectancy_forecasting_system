92609e12a7ac6a551605fef09311ee927d32dc27
RAJ-Git18 <simkhadaraj98@gmail.com>
2026-02-28 19:23:00 +0545

    feat: Implement OCR fallback for PDF text extraction using `pytesseract` and `pdf2image` when direct extraction yields no text.

--- services/data_extraction_service.py
+++ services/data_extraction_service.py
@@ -8,6 +8,8 @@ from schemas.request.predict_lifespan import PredictLifespanRequest
 from dotenv import load_dotenv
 import json
 from langchain_google_genai import ChatGoogleGenerativeAI
+import pytesseract
+from pdf2image import convert_from_bytes
 
 
 load_dotenv()
@@ -25,30 +27,48 @@ class DataExtractionService:
         extracted_text = ""
         for page in pdf:
             extracted_text += page.get_text()
+
+        if len(extracted_text.strip()) == 0:
+            logger.warning(
+                "No text extracted from the PDF so using the OCR as fallback"
+            )
+            return self.extract_data_with_ocr(pdf_bytes=pdf_bytes)
+        logger.info(
+            f"Text extracted from the PDF and its length is: {len(extracted_text.strip())}"
+        )
         return extracted_text
+
+    def extract_data_with_ocr(self, pdf_bytes):
+        images = convert_from_bytes(pdf_file=pdf_bytes)
+        logger.info(f"Total pages: {len(images)}")
+        extracted_text = ""
+        for page, image in enumerate(images):
+            extracted_text += pytesseract.image_to_string(image, lang="eng")
+            logger.info(f"Page {page + 1} completed")
+        return extracted_text.strip()
 
     def get_system_prompt(self) -> str:
         return """You are a data extraction expert. Your task is to extract information from the following text and return it in JSON format as per the schema.
 Example:
 {{
-  "adult_mortality": 10,
-  "infant_deaths": 0,
-  "under_five_deaths": 0,
-  "alcohol": 194,
-  "percentage_expenditure": 0,
-  "hepatitis_b": 553,
-  "measles": 0,
-  "bmi": 34,
-  "polio": 19,
-  "total_expenditure": 226,
-  "diphtheria": 19,
-  "hiv_aids": 0,
-  "gdp": 448,
-  "population": 652,
-  "thinness_1_19_years": 34,
-  "thinness_5_9_years": 34,
-  "income_composition_of_resources": 167,
-  "schooling": 12.4
+"adult_mortality": 10,
+"infant_deaths": 0,
+"under_five_deaths": 0,
+"alcohol": 194,
+"percentage_expenditure": 0,
+"hepatitis_b": 553,
+"measles": 0,
+"bmi": 34,
+"polio": 19,
+"total_expenditure": 226,
+"diphtheria": 19,
+"hiv_aids": 0,
+"gdp": 448,
+"population": 652,
+"thinness_1_19_years": 34,
+"thinness_5_9_years": 34,
+"income_composition_of_resources": 167,
+"schooling": 12.4
 }}
         """
 
@@ -63,7 +83,6 @@ Example:
             ("system", system_content),
             ("human", user_content),
         ]
-        logger.info(f"Prompt ----------> \n{prompt}")
         model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
         structured_model = model.with_structured_output(
             schema=PredictLifespanRequest.model_json_schema(), method="json_schema"
